addb537ada1cbfe317b1e87cb611d1fad0e17e43
tzxdtc <tzxdtc10@gmail.com>
2021-02-28 21:42:19 +0900

    Initial Commit

---
+++ SwiftUITry/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITry
+//
+//  Created by 小牛 on 2021/02/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
